285db06c4effee19e1756ef01a6288198cf04568
AndyFuriousCoder <andrew03.89@mail.ru>
2016-11-25 20:36:42 +0300

    Signed-off-by: AndyFuriousCoder <andrew03.89@mail.ru>

--- src/test/java/testCode/TestBase.java
+++ src/test/java/testCode/TestBase.java
@@ -3,6 +3,8 @@ package testCode;
 import com.epam.jdi.uitests.web.selenium.elements.composite.WebSite;
 import com.epam.jdi.uitests.web.testng.testRunner.TestNGBase;
 import com.epam.web.matcher.verify.Verify;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
@@ -14,6 +16,8 @@ import static pageObjects.pages.TestingEpamSite.homePage;
 
 public class TestBase extends TestNGBase
 {
+
+    public static WebDriver driver = new ChromeDriver();
     @BeforeSuite(alwaysRun = true)
     public static void setUp() throws Exception {
         WebSite.init(TestingEpamSite.class);
